6838d1d40f9db8875d16868c2a349c13da50c33a
ddfunk2 <77017235+ddfunk2@users.noreply.github.com>
2021-01-30 15:02:31 -0500

    Create Pythonwithturtle.py
    These are the basics

---
+++ Pythonwithturtle.py
@@ -0,0 +1,37 @@
+#Import the turtle module
+
+import turtle
+
+#Do something like give the turtle a name
+
+t = turtle.Turtle()
+#My turtles name is t
+
+#Moving the turtle:
+
+t.forward(100)
+#Moves the turtle forward 100 pixels
+
+t.backward(100)
+
+#Same as previous, except it moves the turtle back 100
+
+t.left(90)
+
+#Turns the turtle left 90 degrees. Change the 90 to the amount you want it to turn.
+t.right(90)
+#Opposite of previous
+
+#NOTE: There is NO SUCH COMMAND such as t.up or t.down
+
+t.pendown()
+
+#Puts the pen down
+
+t.penup()
+#Takes the pen up
+
+t.circle(100)
+#Draws a circle with the size of 100. Change the 100 to something else to change the size of your circle
+
+# This is about it!
